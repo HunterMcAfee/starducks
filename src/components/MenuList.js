85d58f0646b31744f58bf10be5cfa3231c79147f
HunterMcAfee <huntmac10@gmail.com>
2017-12-19 18:18:58 -0500

    Added work from meetup

--- src/components/MenuList.js
+++ src/components/MenuList.js
@@ -5,3 +5,19 @@
  * @description This will take a list of items as props
  *
  */
+import React, { Component } from 'react'
+import { FlatList, Text } from 'react-native'
+import menuItems from '../fixtures/menuItems'
+import MenuItem from './MenuItem'
+
+export default class MenuList extends Component {
+    render() {
+        return (
+            <FlatList
+                data={menuItems}
+                keyExtractor={item => item}
+                renderItem={value => <MenuItem item={value.item} />}
+            />
+        )
+    }
+}
